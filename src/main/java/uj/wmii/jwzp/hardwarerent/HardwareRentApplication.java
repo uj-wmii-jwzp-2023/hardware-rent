3db9fd0986f0de5cd1195cdbf97c8d6a31d457be
OleksiiSytnik <oleksii.sytnik@student.uj.edu.pl>
2023-04-12 19:12:53 +0200

    about to merge v2

--- src/main/java/uj/wmii/jwzp/hardwarerent/HardwareRentApplication.java
+++ src/main/java/uj/wmii/jwzp/hardwarerent/HardwareRentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HardwareRentalApplication {
+public class HardwareRentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HardwareRentalApplication.class, args);
+		SpringApplication.run(HardwareRentApplication.class, args);
 	}
 
 }

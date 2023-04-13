424a2455ab08f28304aa17947be31a4200670e12
OleksiiSytnik <oleksii.sytnik@student.uj.edu.pl>
2023-04-13 13:20:47 +0200

    added Orders and OrderDetails (demo v2)

---
+++ src/main/java/uj/wmii/jwzp/hardwarerent/bootstrap/BootstrapConfiguration.java
@@ -0,0 +1,62 @@
+package uj.wmii.jwzp.hardwarerent.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import uj.wmii.jwzp.hardwarerent.data.Category;
+import uj.wmii.jwzp.hardwarerent.data.Product;
+import uj.wmii.jwzp.hardwarerent.repositories.CategoryRepository;
+import uj.wmii.jwzp.hardwarerent.repositories.ProductRepository;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class BootstrapConfiguration implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
+    private final ProductRepository productRepository;
+
+    public BootstrapConfiguration(CategoryRepository categoryRepository, ProductRepository productRepository) {
+        this.categoryRepository = categoryRepository;
+        this.productRepository = productRepository;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Category category_Tv = new Category();
+        category_Tv.setCategoryName("TV");
+        category_Tv = categoryRepository.save(category_Tv);
+
+        Category category_Laptop = new Category();
+        category_Laptop.setCategoryName("Laptop");
+        category_Laptop = categoryRepository.save(category_Laptop);
+
+        Product pr0 = new Product();
+        pr0.setAvailable(true);
+        pr0.setCompanyName("Samsung");
+        pr0.setModel("tv 2000");
+        pr0.setPrice(new BigDecimal("900"));
+        pr0.setCategory(category_Tv);
+        pr0 = productRepository.save(pr0);
+
+        Product pr1 = new Product();
+        pr1.setAvailable(false);
+        pr1.setCompanyName("Dell");
+        pr1.setModel("laptop 2000");
+        pr1.setPrice(new BigDecimal("1000"));
+        pr1.setCategory(category_Laptop);
+        pr1 = productRepository.save(pr1);
+
+        Product pr2 = new Product();
+        pr2.setAvailable(false);
+        pr2.setCompanyName("Acer");
+        pr2.setModel("predator 500");
+        pr2.setPrice(new BigDecimal("1200"));
+        pr2.setCategory(category_Laptop);
+        pr2 = productRepository.save(pr2);
+
+    }
+}
